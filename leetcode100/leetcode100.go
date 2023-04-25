b22c0e571a5059dc1b8abe6ece6150d0ec6aa2d6
litianpeng@37.com <_4WcYPnqVg>
2023-04-25 21:40:05 +0800

    add two exercise

--- leetcode100/leetcode100.go
+++ leetcode100/leetcode100.go
@@ -12,3 +12,44 @@ func twoSum(nums []int, target int) []int {
 	}
 	return nil
 }
+
+// https://leetcode.cn/problems/group-anagrams/?envType=study-plan-v2&id=top-100-liked
+// 49. 字母异位词分组
+func groupAnagrams(strs []string) [][]string {
+	strMap := map[[26]int][]string{}
+	for _, str := range strs {
+		var strArr [26]int
+		for _, s := range str {
+			strArr[s-'a']++
+		}
+		strMap[strArr] = append(strMap[strArr], str)
+	}
+	var res [][]string
+	for _, v := range strMap {
+		res = append(res, v)
+	}
+	return res
+}
+
+// https://leetcode.cn/problems/longest-consecutive-sequence/?envType=study-plan-v2&id=top-100-liked
+// 128. 最长连续序列
+func longestConsecutive(nums []int) int {
+	numMap := map[int]bool{}
+	for _, num := range nums {
+		numMap[num] = true
+	}
+	var max int
+	for _, num := range nums {
+		if !numMap[num-1] {
+			var cur int
+			for numMap[num] {
+				cur++
+				num++
+			}
+			if cur > max {
+				max = cur
+			}
+		}
+	}
+	return max
+}
