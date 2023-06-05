56fbc630207a25fb71a3367e1c3d85d58301c11d
litianpeng@37.com <_4WcYPnqVg>
2023-06-05 20:12:08 +0800

    add some days code

--- everyday/main.go
+++ everyday/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"math"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -67,4 +70,240 @@ func queryString(S string, N int) bool {
 		}
 	}
 	return true
+}
+
+// https://leetcode.cn/problems/distant-barcodes/
+func rearrangeBarcodes(barcodes []int) []int {
+	mmap := make(map[int]int)
+	for _, v := range barcodes {
+		mmap[v]++
+	}
+	var res []int
+	for len(mmap) > 0 {
+		var max, maxK int
+		for k, v := range mmap {
+			if v > max {
+				max = v
+				maxK = k
+			}
+		}
+		delete(mmap, maxK)
+		for i := 0; i < max; i++ {
+			res = append(res, maxK)
+		}
+	}
+	return res
+}
+
+// https://leetcode.cn/problems/flip-columns-for-maximum-number-of-equal-rows/
+func maxEqualRowsAfterFlips(matrix [][]int) int {
+	mmap := make(map[string]int)
+	for _, row := range matrix {
+		var s []int
+		for _, v := range row {
+			s = append(s, v^row[0])
+		}
+		var str strings.Builder
+		for _, v := range s {
+			str.WriteString(strconv.Itoa(v))
+		}
+		mmap[str.String()]++
+	}
+	var res int
+	for _, v := range mmap {
+		if v > res {
+			res = v
+		}
+	}
+	return res
+}
+
+// https://leetcode.cn/problems/statistics-from-a-large-sample/
+// 输入：count = [0,1,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+// 输出：[1.00000,3.00000,2.37500,2.50000,3.00000]
+func sampleStats(count []int) []float64 {
+	n := len(count)
+	total := 0
+	for i := 0; i < n; i++ {
+		total += count[i]
+	}
+	mean := 0.0
+	median := 0.0
+	minimum := 256
+	maxnum := 0
+	mode := 0
+
+	left := (total + 1) / 2
+	right := (total + 2) / 2
+	cnt := 0
+	maxfreq := 0
+	sum := 0
+	for i := 0; i < n; i++ {
+		sum += int(count[i]) * int(i)
+		if count[i] > maxfreq {
+			maxfreq = count[i]
+			mode = i
+		}
+		if count[i] > 0 {
+			if minimum == 256 {
+				minimum = i
+			}
+			maxnum = i
+		}
+		if cnt < right && cnt+count[i] >= right {
+			median += float64(i)
+		}
+		if cnt < left && cnt+count[i] >= left {
+			median += float64(i)
+		}
+		cnt += count[i]
+	}
+	mean = float64(sum) / float64(total)
+	median = median / 2.0
+	return []float64{float64(minimum), float64(maxnum), mean, median, float64(mode)}
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// https://leetcode.cn/problems/delete-nodes-and-return-forest/
+func delNodes(root *TreeNode, to_delete []int) []*TreeNode {
+	mmap := make(map[int]bool)
+	for _, v := range to_delete {
+		mmap[v] = true
+	}
+	var res []*TreeNode
+	var dfs func(*TreeNode, bool) *TreeNode
+	dfs = func(node *TreeNode, isRoot bool) *TreeNode {
+		if node == nil {
+			return nil
+		}
+		deleted := false
+		if mmap[node.Val] {
+			deleted = true
+		}
+		if isRoot && !deleted {
+			res = append(res, node)
+		}
+		node.Left = dfs(node.Left, deleted)
+		node.Right = dfs(node.Right, deleted)
+		if deleted {
+			return nil
+		}
+		return node
+	}
+	dfs(root, true)
+	return res
+}
+
+// https://leetcode.cn/problems/minimum-cost-tree-from-leaf-values/
+func mctFromLeafValues(arr []int) int {
+	n := len(arr)
+	var res int
+	var stack []int
+	stack = append(stack, math.MaxInt32)
+	for i := 0; i < n; i++ {
+		for stack[len(stack)-1] <= arr[i] {
+			tmp := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res += tmp * min(stack[len(stack)-1], arr[i])
+		}
+		stack = append(stack, arr[i])
+	}
+	for len(stack) > 2 {
+		tmp := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res += tmp * stack[len(stack)-1]
+	}
+	return res
+}
+
+// https://leetcode.cn/problems/maximum-tastiness-of-candy-basket/
+func maximumTastiness(price []int, k int) int {
+	sort.Ints(price)
+	left, right := 0, price[len(price)-1]-price[0]
+	for left < right {
+		mid := (left + right + 1) / 2
+		if check(price, k, mid) {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
+
+func check(price []int, k int, tastiness int) bool {
+	prev := int(math.Inf(-1)) >> 1
+	cnt := 0
+	for _, p := range price {
+		if p-prev >= tastiness {
+			cnt++
+			prev = p
+		}
+	}
+	return cnt >= k
+}
+
+// https://leetcode.cn/problems/count-vowel-strings-in-ranges/
+func vowelStrings(words []string, queries [][]int) []int {
+	n := len(words)
+	prefixSums := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		value := 0
+		if checkStringHasPrefixAndSuffixVowel(words[i]) {
+			value = 1
+		}
+		prefixSums[i+1] = prefixSums[i] + value
+	}
+	ans := make([]int, len(queries))
+	for i := 0; i < len(queries); i++ {
+		start := queries[i][0]
+		end := queries[i][1]
+		ans[i] = prefixSums[end+1] - prefixSums[start]
+	}
+	return ans
+}
+
+func checkStringHasPrefixAndSuffixVowel(w string) bool {
+	if len(w) == 0 {
+		return false
+	}
+	mmap := make(map[byte]bool)
+	mmap['a'] = true
+	mmap['e'] = true
+	mmap['i'] = true
+	mmap['o'] = true
+	mmap['u'] = true
+	return mmap[w[0]] && mmap[w[len(w)-1]]
+}
+
+// https://leetcode.cn/problems/apply-operations-to-an-array/
+func applyOperations(nums []int) []int {
+	n := len(nums)
+	for i := 0; i < n-1; i++ {
+		if nums[i] == nums[i+1] {
+			nums[i] = nums[i] * 2
+			nums[i+1] = 0
+		}
+	}
+	res := make([]int, n)
+	j := 0
+	for i := 0; i < n; i++ {
+		if nums[i] != 0 {
+			res[j] = nums[i]
+			j++
+		}
+	}
+	return res
+}
+
+func main() {
+	//maxEqualRowsAfterFlips([][]int{{0, 0, 0}, {0, 0, 1}, {1, 1, 0}})
+	words := []string{"bzmxvzjxfddcuznspdcbwiojiqf", "mwguoaskvramwgiweogzulcinycosovozppl", "uigevazgbrddbcsvrvnngfrvkhmqszjicpieahs", "uivcdsboxnraqpokjzaayedf", "yalc", "bbhlbmpskgxmxosft", "vigplemkoni", "krdrlctodtmprpxwditvcps", "gqjwokkskrb", "bslxxpabivbvzkozzvdaykaatzrpe", "qwhzcwkchluwdnqjwhabroyyxbtsrsxqjnfpadi", "siqbezhkohmgbenbkikcxmvz", "ddmaireeouzcvffkcohxus", "kjzguljbwsxlrd", "gqzuqcljvcpmoqlnrxvzqwoyas", "vadguvpsubcwbfbaviedr", "nxnorutztxfnpvmukpwuraen", "imgvujjeygsiymdxp", "rdzkpk", "cuap", "qcojjumwp", "pyqzshwykhtyzdwzakjejqyxbganow", "cvxuskhcloxykcu", "ul", "axzscbjajazvbxffrydajapweci"}
+	queries := [][]int{{4, 4}, {6, 17}, {10, 17}, {9, 18}, {17, 22}, {5, 23}, {2, 5}, {17, 21}, {5, 17}, {4, 8}, {7, 17}, {16, 19}, {7, 12}, {9, 20}, {13, 23}, {1, 5}, {19, 19}}
+	fmt.Println(vowelStrings(words, queries))
 }
