a6aecc40fcaa76e2f4b7836bf5c8a0eca3f28375
litianpeng@37.com <_4WcYPnqVg>
2023-09-01 22:43:06 +0800

    add new

--- everyday/main.go
+++ everyday/main.go
@@ -450,6 +450,21 @@ func minFallingPathSum(matrix [][]int) int {
 	return res
 }
 
+// https://leetcode.cn/problems/number-of-ways-to-buy-pens-and-pencils/?envType=daily-question&envId=2023-09-01
+// 给你一个整数 total ，表示你拥有的总钱数。同时给你两个整数 cost1 和 cost2 ，分别表示一支钢笔和一支铅笔的价格。你可以花费你部分或者全部的钱，去买任意数目的两种笔。
+// 请你返回购买钢笔和铅笔的 不同方案数目 。
+func waysToBuyPensPencils(total int, cost1 int, cost2 int) int64 {
+	if cost1 < cost2 {
+		return waysToBuyPensPencils(total, cost2, cost1)
+	}
+	var res, cnt int
+	for cnt*cost1 <= total {
+		res += (total-cnt*cost1)/cost2 + 1
+		cnt++
+	}
+	return int64(res)
+}
+
 func main() {
 	//maxEqualRowsAfterFlips([][]int{{0, 0, 0}, {0, 0, 1}, {1, 1, 0}})
 	words := []string{"bzmxvzjxfddcuznspdcbwiojiqf", "mwguoaskvramwgiweogzulcinycosovozppl", "uigevazgbrddbcsvrvnngfrvkhmqszjicpieahs", "uivcdsboxnraqpokjzaayedf", "yalc", "bbhlbmpskgxmxosft", "vigplemkoni", "krdrlctodtmprpxwditvcps", "gqjwokkskrb", "bslxxpabivbvzkozzvdaykaatzrpe", "qwhzcwkchluwdnqjwhabroyyxbtsrsxqjnfpadi", "siqbezhkohmgbenbkikcxmvz", "ddmaireeouzcvffkcohxus", "kjzguljbwsxlrd", "gqzuqcljvcpmoqlnrxvzqwoyas", "vadguvpsubcwbfbaviedr", "nxnorutztxfnpvmukpwuraen", "imgvujjeygsiymdxp", "rdzkpk", "cuap", "qcojjumwp", "pyqzshwykhtyzdwzakjejqyxbganow", "cvxuskhcloxykcu", "ul", "axzscbjajazvbxffrydajapweci"}
