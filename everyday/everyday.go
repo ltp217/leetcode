b4dc514f5baa84167f137e0284de3fbb7246de46
litianpeng@37.com <_4WcYPnqVg>
2023-09-21 20:19:56 +0800

    add 20230921

--- everyday/everyday.go
+++ everyday/everyday.go
@@ -10,6 +10,107 @@ import (
 	"unicode"
 )
 
+// https://leetcode.cn/problems/collect-coins-in-a-tree/?envType=daily-question&envId=2023-09-21
+func collectTheCoins(coins []int, edges [][]int) int {
+	n := len(coins)
+	g := make([][]int, n) // 建图
+	deg := make([]int, n) // 度数
+	for _, e := range edges {
+		x, y := e[0], e[1]
+		g[x] = append(g[x], y)
+		g[y] = append(g[y], x)
+		deg[x]++
+		deg[y]++
+	}
+	leftEdges := n - 1 // 剩余边数
+	// 拓扑排序，去掉没有金币的子树
+	q := []int{}
+	for i, d := range deg {
+		if d == 1 && coins[i] == 0 {
+			q = append(q, i)
+		}
+	}
+	for len(q) > 0 {
+		x := q[len(q)-1]
+		q = q[:len(q)-1]
+		leftEdges-- // 删除节点x到父节点的边
+		for _, y := range g[x] {
+			deg[y]--
+			if deg[y] == 1 && coins[y] == 0 { // 没有金币的叶子
+				q = append(q, y)
+			}
+		}
+	}
+	// 再次拓扑排序
+	for i, d := range deg {
+		if d == 1 && coins[i] > 0 {
+			q = append(q, i)
+		}
+	}
+	leftEdges -= len(q)   // 删除所有的叶子节点
+	for _, x := range q { // 遍历所有的叶子节点
+		for _, y := range g[x] {
+			deg[y]--
+			if deg[y] == 1 { // 现在是叶子节点了
+				leftEdges-- // 删除y其父节点的
+			}
+		}
+	}
+	if leftEdges < 0 {
+		return 0
+	}
+	return 2 * leftEdges
+}
+
+// https://leetcode.cn/problems/na-ying-bi/description/?envType=daily-question&envId=2023-09-20
+func minCount(coins []int) int {
+	res := 0
+	for _, c := range coins {
+		a := c / 2
+		if a == 0 {
+			res += 1
+		} else {
+			if c%2 == 0 {
+				res += a
+			} else {
+				res += a + 1
+			}
+		}
+	}
+	return res
+}
+
+// https://leetcode.cn/problems/house-robber-iv/description/?envType=daily-question&envId=2023-09-19
+func minCapability(nums []int, k int) int {
+	lower, upper := nums[0], nums[0]
+	for _, x := range nums {
+		if x < lower {
+			lower = x
+		}
+		if x > upper {
+			upper = x
+		}
+	}
+	for lower <= upper {
+		mid := (lower + upper) / 2
+		count, visited := 0, false
+		for _, x := range nums {
+			if x <= mid && !visited {
+				count++
+				visited = true
+			} else {
+				visited = false
+			}
+		}
+		if count >= k {
+			upper = mid - 1
+		} else {
+			lower = mid + 1
+		}
+	}
+	return lower
+}
+
 // https://leetcode.cn/problems/house-robber-iii/description/?envType=daily-question&envId=2023-09-18
 func rob3(root *TreeNode) int {
 	val := dfsrob3(root)
