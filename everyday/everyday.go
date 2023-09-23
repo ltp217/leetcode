4906f82cca3788d505ad2a980f9cc868f8ba0ef5
litianpeng@37.com <_4WcYPnqVg>
2023-09-23 22:05:52 +0800

    add 20230923

--- everyday/everyday.go
+++ everyday/everyday.go
@@ -10,6 +10,105 @@ import (
 	"unicode"
 )
 
+// https://leetcode.cn/problems/operations-on-tree/description/?envType=daily-question&envId=2023-09-23
+type LockingTree struct {
+	parent       []int
+	lockNodeUser []int
+	children     [][]int
+}
+
+func Constructor(parent []int) LockingTree {
+	n := len(parent)
+	lockNodeUser := make([]int, n)
+	child := make([][]int, n)
+	for i := 0; i < n; i++ {
+		lockNodeUser[i] = -1
+		p := parent[i]
+		if p != -1 {
+			child[p] = append(child[p], i)
+		}
+	}
+	return LockingTree{
+		parent:       parent,
+		lockNodeUser: lockNodeUser,
+		children:     child,
+	}
+}
+
+func (this *LockingTree) Lock(num int, user int) bool {
+	if this.lockNodeUser[num] == -1 {
+		this.lockNodeUser[num] = user
+		return true
+	}
+	return false
+}
+
+func (this *LockingTree) Unlock(num int, user int) bool {
+	if this.lockNodeUser[num] == user {
+		this.lockNodeUser[num] = -1
+		return true
+	}
+	return false
+}
+
+func (this *LockingTree) Upgrade(num int, user int) bool {
+	res := this.lockNodeUser[num] == -1 && !this.hasLockedAncestor(num) && this.checkAndUnlockDescendant(num)
+	if res {
+		this.lockNodeUser[num] = user
+	}
+	return res
+}
+
+func (this *LockingTree) hasLockedAncestor(num int) bool {
+	num = this.parent[num]
+	for num != -1 {
+		if this.lockNodeUser[num] != -1 {
+			return true
+		}
+		num = this.parent[num]
+	}
+	return false
+}
+
+func (this *LockingTree) checkAndUnlockDescendant(num int) bool {
+	res := false
+	if this.lockNodeUser[num] != -1 {
+		res = true
+	}
+	this.lockNodeUser[num] = -1
+	for _, child := range this.children[num] {
+		if this.checkAndUnlockDescendant(child) {
+			res = true
+		}
+	}
+	return res
+}
+
+/**
+ * Your LockingTree object will be instantiated and called as such:
+ * obj := Constructor(parent);
+ * param_1 := obj.Lock(num,user);
+ * param_2 := obj.Unlock(num,user);
+ * param_3 := obj.Upgrade(num,user);
+ */
+// https://leetcode.cn/problems/distribute-money-to-maximum-children/description/?envType=daily-question&envId=2023-09-22
+func distMoney(money int, children int) int {
+	if money < children {
+		return -1
+	}
+	money -= children
+	cnt := money / 7
+	if children < cnt {
+		cnt = children
+	}
+	money -= cnt * 7
+	children -= cnt
+	if (children == 0 && money > 0) || (children == 1 && money%3 == 0) {
+		cnt -= 1
+	}
+	return cnt
+}
+
 // https://leetcode.cn/problems/collect-coins-in-a-tree/?envType=daily-question&envId=2023-09-21
 func collectTheCoins(coins []int, edges [][]int) int {
 	n := len(coins)
@@ -920,7 +1019,8 @@ func checkIfPrerequisite(numCourses int, prerequisites [][]int, queries [][]int)
 }
 
 func main() {
-	fmt.Println(captureForts([]int{1, 0, 0, -1, 0, 0, 0, 0, 1}))
+	fmt.Println(distMoney(16, 2))
+	//fmt.Println(captureForts([]int{1, 0, 0, -1, 0, 0, 0, 0, 1}))
 	//maxEqualRowsAfterFlips([][]int{{0, 0, 0}, {0, 0, 1}, {1, 1, 0}})
 	//words := []string{"bzmxvzjxfddcuznspdcbwiojiqf", "mwguoaskvramwgiweogzulcinycosovozppl", "uigevazgbrddbcsvrvnngfrvkhmqszjicpieahs", "uivcdsboxnraqpokjzaayedf", "yalc", "bbhlbmpskgxmxosft", "vigplemkoni", "krdrlctodtmprpxwditvcps", "gqjwokkskrb", "bslxxpabivbvzkozzvdaykaatzrpe", "qwhzcwkchluwdnqjwhabroyyxbtsrsxqjnfpadi", "siqbezhkohmgbenbkikcxmvz", "ddmaireeouzcvffkcohxus", "kjzguljbwsxlrd", "gqzuqcljvcpmoqlnrxvzqwoyas", "vadguvpsubcwbfbaviedr", "nxnorutztxfnpvmukpwuraen", "imgvujjeygsiymdxp", "rdzkpk", "cuap", "qcojjumwp", "pyqzshwykhtyzdwzakjejqyxbganow", "cvxuskhcloxykcu", "ul", "axzscbjajazvbxffrydajapweci"}
 	//queries := [][]int{{4, 4}, {6, 17}, {10, 17}, {9, 18}, {17, 22}, {5, 23}, {2, 5}, {17, 21}, {5, 17}, {4, 8}, {7, 17}, {16, 19}, {7, 12}, {9, 20}, {13, 23}, {1, 5}, {19, 19}}
