90350a55a267ac53446c70552e086bcb2fb6659d
litianpeng <litianpeng@37.com>
2023-10-15 09:59:43 +0800

    add 20231015

--- everyday/everyday.go
+++ everyday/everyday.go
@@ -10,6 +10,21 @@ import (
 	"unicode"
 )
 
+// https://leetcode.cn/problems/single-number-ii/description/?envType=daily-question&envId=2023-10-15
+func singleNumber2(nums []int) int {
+	ans := int32(0)
+	for i := 0; i < 32; i++ {
+		total := int32(0)
+		for _, num := range nums {
+			total += int32(num) >> i & 1
+		}
+		if total%3 > 0 {
+			ans |= 1 << i
+		}
+	}
+	return int(ans)
+}
+
 // https://leetcode.cn/problems/single-number/description/?envType=daily-question&envId=2023-10-14
 func singleNumber(nums []int) int {
 	res := 0
