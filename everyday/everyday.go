79b00399082ecd8039e565dfab385c12e90f6c8e
litianpeng <litianpeng@37.com>
2024-03-04 20:14:46 +0800

    update 20240304

--- everyday/everyday.go
+++ everyday/everyday.go
@@ -10,6 +10,88 @@ import (
 	"unicode"
 )
 
+// https://leetcode.cn/problems/implement-queue-using-stacks/?envType=daily-question&envId=2024-03-04
+type MyQueue struct {
+	stack1 []int
+	stack2 []int
+}
+
+func ConstructorMyQueue() MyQueue {
+	return MyQueue{}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.stack1 = append(this.stack1, x)
+}
+
+func (this *MyQueue) Pop() int {
+	if len(this.stack2) == 0 {
+		for len(this.stack1) > 0 {
+			this.stack2 = append(this.stack2, this.stack1[len(this.stack1)-1])
+			this.stack1 = this.stack1[:len(this.stack1)-1]
+		}
+	}
+	res := this.stack2[len(this.stack2)-1]
+	this.stack2 = this.stack2[:len(this.stack2)-1]
+	return res
+}
+
+func (this *MyQueue) Peek() int {
+	if len(this.stack2) == 0 {
+		for len(this.stack1) > 0 {
+			this.stack2 = append(this.stack2, this.stack1[len(this.stack1)-1])
+			this.stack1 = this.stack1[:len(this.stack1)-1]
+		}
+	}
+	return this.stack2[len(this.stack2)-1]
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.stack1) == 0 && len(this.stack2) == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
+
+// https://leetcode.cn/problems/implement-stack-using-queues/description/?envType=daily-question&envId=2024-03-03
+type MyStack struct {
+	arr1 []int
+	arr2 []int
+}
+
+func ConstructorMyStack() MyStack {
+	return MyStack{}
+}
+
+func (this *MyStack) Push(x int) {
+	this.arr2 = append(this.arr2, x)
+	for len(this.arr1) > 0 {
+		this.arr2 = append(this.arr2, this.arr1[0])
+		this.arr1 = this.arr1[1:]
+	}
+	this.arr1, this.arr2 = this.arr2, this.arr1
+}
+
+func (this *MyStack) Pop() int {
+	res := this.arr1[0]
+	this.arr1 = this.arr1[1:]
+	return res
+}
+
+func (this *MyStack) Top() int {
+	return this.arr1[0]
+}
+
+func (this *MyStack) Empty() bool {
+	return len(this.arr1) == 0
+}
+
 // https://leetcode.cn/problems/maximum-subarray/description/?envType=daily-question&envId=2023-11-20
 func maxSubArray(nums []int) int { // 前缀和
 	n := len(nums)
